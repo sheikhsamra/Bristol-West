313b37760eae2eb7d8a388dfaf196a7f036126d3
Samra <shaikhsamra855@gmail.com>
2026-05-27 03:08:42 +0500

    add backend and admin dashboard

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
     dbName: 'Insurancewebsite'
