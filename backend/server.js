a78e295091d9904e76ed158af3e6017f2cee4d9e
Samra <shaikhsamra855@gmail.com>
2026-05-27 01:36:45 +0500

    add backend and admin dashboard

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://bristol-west.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Root Route for health check
